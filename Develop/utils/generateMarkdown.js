fe50d6121b5f36bf1f49eab68b9a11d18b8b2178
tbreazier <64601341+tbreazier@users.noreply.github.com>
2020-07-19 13:49:19 -0500

    added MD format to generateMD function

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,6 +1,38 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `
+  
+  # ${data.title}
+
+  ### Description
+  ${data.about}
+
+  ### Table of Contents
+  +[Installation](#installation)
+  +[Usage](#usage)
+  +[Credits](#credits)
+  +[License](#license)
+  +[Tests](#tests)
+  +[Questions?](#questions?)
+
+  ### Installation
+  ${data.install}
+
+  ### Usage
+  ${data.usage}
+
+  ### Credits
+  ${data.credit}
+
+  ### License
+  ${data.license}
+
+  ### Tests
+  ${data.test}
+
+  ### Questions?
+  Email: ${data.email}
+  Github: ${data.github}
 
 `;
 }
--- README.md
+++ README.md
@@ -3,12 +3,12 @@
 ### Description
 
 ### Table of Contents
-1. [Installation](#installation)
-2. [Usage](#usage)
-3. [Credits](#credits)
-4. [License](#license)
-5. [Tests](#tests)
-6. [Questions?](#questions?)
++[Installation](#installation)
++[Usage](#usage)
++[Credits](#credits)
++[License](#license)
++[Tests](#tests)
++[Questions?](#questions?)
 
 ### Installation
 
@@ -21,3 +21,5 @@
 ### Tests
 
 ### Questions?
+Email:
+Github: 
