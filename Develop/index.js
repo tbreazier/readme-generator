f698f06f5a0bc049b5d56835c0d30d3e3a76b0f8
tbreazier <64601341+tbreazier@users.noreply.github.com>
2020-07-19 12:11:46 -0500

    added question template

--- Develop/index.js
+++ Develop/index.js
@@ -1,6 +1,21 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
+
 // array of questions for user
 const questions = [
-
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the name of your project? (Required)',
+        validate: nameInput => {
+            if (nameInput) {
+              return true;
+            } else {
+              console.log('Please enter your name!');
+              return false;
+            }
+        }
+    },
 ];
 
 // function to write README file
